9e18c802c27c661295fb1e72f985007ad547402e
KarlosHenryque <karloshenryquep@gmail.com>
2026-05-18 22:44:13 -0300

    Tipos primitivos

---
+++ maratona.java/src/academy.devdojo.maratonjava.introducao/Aula01OlaDevDojo.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonjava.introducao;
+
+//Comentário em linha
+
+/*
+Comentário em bloco
+*/
+
+public class Aula01OlaDevDojo {
+    public static void main (String[] args) {
+        System.out.println("Hello my friend !");
+    }
+}
---
+++ maratona.java/src/academy.devdojo.maratonjava.introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,22 @@
+package academy.devdojo.maratonjava.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        System.out.println("Tipo Primitivos");
+
+        //int, double, float, char, byte,short, long, boolean
+
+        int age = 10;
+        System.out.println("Idade: " + age);
+
+        double height = 1.75;
+        System.out.println("Tamanho: " + height);
+
+        float wage = 1.800f;
+        System.out.println("Salário: " + wage);
+
+        boolean status = true;
+        System.out.println("Esta vivo: " + status);
+
+    }
+}
