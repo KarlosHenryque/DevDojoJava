a814d61ec579e9ac8ad7ef4bbebaa6e1900b6678
KarlosHenryque <karloshenryquep@gmail.com>
2026-05-26 22:30:44 -0300

    Introdução

--- maratona.java/src/academy.devdojo.maratonjava.introducao/Aula04Operadores.java
+++ maratona.java/src/academy.devdojo.maratonjava.introducao/Aula04Operadores.java
@@ -38,5 +38,50 @@ public class Aula04Operadores {
         System.out.println("Dez é menor igual a vinte ? " +isDezMenorIgualQueVinte);
         System.out.println("Dez é igual a vinte ? " +isDezIgualAVinte);
         System.out.println("Dez é diferente de vinte ? " +isDezDiferenteDeVinte);
+
+        // && (AND),  || (OR),  ! (NOT)
+
+        int idade = 29;
+        float salario = 3500F;
+        boolean isDentroDaLeiMaiorQueTrinta = idade >= 30 && salario > 4612;
+        boolean isDentroDaLeiMenorQueTrinta = idade < 30 && salario >= 3381;
+
+        System.out.println("Operador AND");
+        System.out.println("\nDentro da lei maior que trinta ?" +isDentroDaLeiMaiorQueTrinta);
+        System.out.println("Dentro da lei menor que trinta ?" +isDentroDaLeiMenorQueTrinta);
+
+        double valorTotalContaCorrente = 200;
+        double valorTotalContaPoupanca = 10000;
+        float valorPlaystation = 5000F;
+
+        boolean isPlaystationCincoCompravel = valorTotalContaCorrente > valorPlaystation || valorTotalContaPoupanca > valorPlaystation;
+
+        System.out.println("Operador OR");
+        System.out.println("\nViável comprar o playstation 5 ? "+isPlaystationCincoCompravel);
+
+        // Operador de atribuição ( =, +=, -=, *=, /=, %=)
+
+        double bonus = 1800;
+        bonus += 1000;
+        System.out.println("\nBonus: " +bonus);
+
+        bonus -= 500;
+        System.out.println("Bonus: " +bonus);
+
+        bonus *= 500;
+        System.out.println("Bonus: " +bonus);
+
+        bonus /= 500;
+        System.out.println("Bonus: " +bonus);
+
+        bonus %= 500;
+        System.out.println("Bonus: " +bonus);
+
+        int contador = 0;
+        contador++;
+        System.out.println("\nContador: "+ contador);
+        contador--;
+        System.out.println("Contador: "+ contador);
+
     }
 }
