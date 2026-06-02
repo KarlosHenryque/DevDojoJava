c45f127f418e5f02c3520a7ba640c323a59d733a
KarlosHenryque <karloshenryquep@gmail.com>
2026-06-01 22:10:04 -0300

    Else, else if e ternario

---
+++ maratona.java/src/academy.devdojo.maratonjava.introducao/aula05EstruturasCondicionais03.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonjava.introducao;
+
+public class aula05EstruturasCondicionais03 {
+    public static void main(String[] args) {
+
+        double salario = 6000;
+        String mensagemDoar = "Eu vou doar 500 para o DevDojo";
+        String mensagemNaoDoar = "Ainda não tenho condição, mas vou ter!";
+
+        // (condição) ? verdadeiro : falso
+        String Resultado = salario > 5000 ? mensagemDoar : mensagemNaoDoar;
+
+        System.out.println(Resultado);
+    }
+}
