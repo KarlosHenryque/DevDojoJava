ae597f98245f0b02a1947b2ff08313cfdb5125e5
KarlosHenryque <karloshenryquep@gmail.com>
2026-06-14 11:24:44 -0300

    Arrays

---
+++ maratona.java/src/academy.devdojo.maratonjava.introducao/Aula07Arrays03.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonjava.introducao;
+
+public class Aula07Arrays03 {
+    public static void main(String[] args) {
+        int[] numeros = {1,2,3,4,5};
+        int[] numeros2 = new int[]{1,2,3,4,5};
+
+        // Foreach
+        for (int numero : numeros){
+            System.out.println(numero);
+        }
+        
+    }
+}
