74fbb1d7de0fe3d39dd016166fa467e4fce727ca
KarlosHenryque <karloshenryquep@gmail.com>
2026-06-22 21:50:04 -0300

    Metodos

---
+++ maratona.java/src/academy/devdojo/maratonjava/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -0,0 +1,19 @@
+package academy.devdojo.maratonjava.javacore.Bintroducaometodos.dominio;
+
+public class Calculadora {
+
+    public void somaDoisNumeros() {
+        System.out.println("Soma");
+        System.out.println(10 + 10);
+    }
+
+    public void subtraiDoisNumeros() {
+        System.out.println("\nSubtrai");
+        System.out.println(10 - 2);
+    }
+
+    public void multiplicaDoisNumeros(int num1, int num2) {
+        System.out.println("\nMultiplicar");
+        System.out.println(num1 * num2);
+    }
+}
