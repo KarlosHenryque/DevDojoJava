c3a8ddc94482c8ca3d9db3d7f11be89b89dbfa59
KarlosHenryque <karloshenryquep@gmail.com>
2026-07-08 21:18:28 -0300

    Métodos estáticos

--- maratona.java/src/academy/devdojo/maratonjava/javacore/Dconstrutores/Dominio/Anime.java
+++ maratona.java/src/academy/devdojo/maratonjava/javacore/Dconstrutores/Dominio/Anime.java
@@ -5,13 +5,23 @@ public class Anime {
     private String tipo;
     private int episodios;
     private String genero;
+    private String estudio;
 
     public Anime(String nome, String tipo, int episodios, String genero) {
-        System.out.println("Dentro do construtor");
+        this();
         this.nome = nome;
         this.tipo = tipo;
         this.episodios = episodios;
         this.genero = genero;
+    }
+
+    public Anime(String nome, String tipo, int episodios, String genero, String estudio) {
+        this(nome, tipo, episodios, genero);
+        this.estudio = estudio;
+    }
+
+    public Anime() {
+        System.out.println("Dentro do construtor");
     }
 
     public void setNome(String nome) {
@@ -51,5 +61,6 @@ public class Anime {
         System.out.println("Tipo: "+this.tipo);
         System.out.println("Episódios: "+this.episodios);
         System.out.println("Gênero: "+this.genero);
+        System.out.println("Estudio: "+this.estudio+ "\n");
     }
 }
--- maratona.java/src/academy/devdojo/maratonjava/javacore/Dconstrutores/teste/AnimeTest01.java
+++ maratona.java/src/academy/devdojo/maratonjava/javacore/Dconstrutores/teste/AnimeTest01.java
@@ -4,11 +4,13 @@ import academy.devdojo.maratonjava.javacore.Dconstrutores.Dominio.Anime;
 
 public class AnimeTest01 {
     public static void main(String[] args) {
-        Anime anime = new Anime("Haikyuu", "tv", 12, "Ação\n");
+        Anime anime = new Anime("Haikyuu", "tv", 12, "Ação");
         Anime anime02 = new Anime("Death Note", "HBO", 24, "Terror");
+        Anime anime03 = new Anime("Death Note", "HBO", 24, "Terror", "03");
 
         anime.imprime();
         anime02.imprime();
+        anime03.imprime();
 
 
     }
