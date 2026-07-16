b483243c84313d0765c3c66737c9805492774eea
KarlosHenryque <karloshenryquep@gmail.com>
2026-07-16 16:24:03 -0300

    Atributos

--- maratona.java/src/academy/devdojo/maratonjava/javacore/Kenum/teste/ClienteTeste01.java
+++ maratona.java/src/academy/devdojo/maratonjava/javacore/Kenum/teste/ClienteTeste01.java
@@ -2,13 +2,20 @@ package academy.devdojo.maratonjava.javacore.Kenum.teste;
 
 import academy.devdojo.maratonjava.javacore.Kenum.dominio.Cliente;
 import academy.devdojo.maratonjava.javacore.Kenum.dominio.TipoCliente;
+import academy.devdojo.maratonjava.javacore.Kenum.dominio.TipoPagamento;
 
 public class ClienteTeste01 {
     public static void main(String[] args) {
-        Cliente cliente1 = new Cliente("Tsubasa", TipoCliente.PESSOA_JURIDICA, Cliente.TipoPagamento.DEBITO);
-        Cliente cliente2 = new Cliente("Akira", TipoCliente.PESSOA_FISICA, Cliente.TipoPagamento.CREDITO);
+        Cliente cliente1 = new Cliente("Tsubasa", TipoCliente.PESSOA_JURIDICA, TipoPagamento.DEBITO);
+        Cliente cliente2 = new Cliente("Akira", TipoCliente.PESSOA_FISICA, TipoPagamento.CREDITO);
 
         System.out.println(cliente1);
         System.out.println(cliente2);
+        System.out.println(TipoPagamento.DEBITO.calcularDesconto(100));
+        System.out.println(TipoPagamento.CREDITO.calcularDesconto(100));
+        TipoCliente tipoCliente01 = TipoCliente.valueOf("PESSOA_FISICA");
+        System.out.println(tipoCliente01);
+        TipoCliente tipoCliente02 = TipoCliente.tipoClientePorNomeRelatorio("Pessoa Física");
+        System.out.println(tipoCliente02);
     }
 }
